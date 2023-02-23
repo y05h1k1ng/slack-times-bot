11cedd9836ff0c4f3234013b00e15921750daa79
yoshiking <yoshiking0711@gmail.com>
2023-02-23 18:38:04 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,56 @@
+import os
+import datetime
+
+from slack_sdk import WebClient
+
+client = WebClient(token=os.environ["SLACK_USER_TOKEN"]) # unarchiveを使用するために、Bot tokenではなくUser OAurh Tokenを使用
+today = datetime.datetime.now()
+times_channel = "times-{}"
+user_id = "U0325Q5U6KT"
+
+filtered_channels = [
+    datetime.timedelta(days=0),
+    datetime.timedelta(days=1),
+    datetime.timedelta(days=3),
+    datetime.timedelta(weeks=1),
+    datetime.timedelta(weeks=2),
+    datetime.timedelta(weeks=4),
+    datetime.timedelta(weeks=8)
+]
+filtered_channel_names = [times_channel.format((today-delta).strftime("%Y%m%d")) for delta in filtered_channels]
+
+def create_today_channel():
+    today_channel = times_channel.format(today.strftime("%Y%m%d"))
+    create_response = client.conversations_create(name=today_channel)
+    print("[+] create:", create_response)
+    invite_response = client.conversations_invite(
+        channel=create_response["channel"]["id"],
+        users=user_id
+    )
+    print("[+] invite:", invite_response)
+    return
+
+def filter_channel():
+    list_response = client.conversations_list()
+    for channel in list_response["channels"]:
+        channel_name = channel["name"]
+        if not channel_name.startswith("times-"):
+            # times channel以外は無視
+            continue
+    
+        if channel_name in filtered_channel_names:
+            # unarchive
+            if not channel["is_archived"]:
+                continue
+            unarchive_response = client.conversations_unarchive(channel=channel["id"])
+            print("[+] unarchive:", channel_name, unarchive_response)
+        else:
+            # archive
+            if channel["is_archived"]:
+                continue
+            archive_response = client.conversations_archive(channel=channel["id"])
+            print("[+] archive:", channel_name, archive_response)
+    return
+
+create_today_channel()
+filter_channel()
